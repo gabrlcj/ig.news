0764386aaaadd2b6d104764bd1ca2e40313b6771
Gabriel <gabrielbittencourt57@gmail.com>
2021-10-22 17:07:33 -0300

    :sparkles: feat: Added functionality to the button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image'
+
 import { SignInButton } from '../SignInButton'
+
+import logoImg from '/public/images/logo.svg'
 import styles from './styles.module.scss'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg' alt='ig.news' />
+        <Image src={logoImg} alt='ig.news' />
         <nav>
           <a className={styles.active} href='#'>
             Home
--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,20 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signin, signout, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type='button' className={styles.signInButton}>
+  return session ? (
+    <button type='button' className={styles.signInButton} onClick={() => signout()}>
       <FaGithub size={20} color='#04d361' />
-      Gabriel Bittencourt
+      {session.user.name}
       <FiX className={styles.close} size={20} color='#737380' />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton}>
+    <button type='button' className={styles.signInButton} onClick={() => signin('Github')}>
       <FaGithub size={20} color='#eba417' />
       Sign In with Github
     </button>
