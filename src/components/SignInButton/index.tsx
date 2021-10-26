3073b566d6c7bd26659109df5fc936e17155e967
Gabriel <gabrielbittencourt57@gmail.com>
2021-10-26 00:57:41 -0300

    :card_file_box: feat: Added HTTP request POST to subscribe button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -8,13 +8,21 @@ export function SignInButton() {
   const [session] = useSession()
 
   return session ? (
-    <button type='button' className={styles.signInButton} onClick={() => signout()}>
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signout()}
+    >
       <FaGithub size={20} color='#04d361' />
       {session.user.name}
       <FiX className={styles.close} size={20} color='#737380' />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton} onClick={() => signin('Github')}>
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signin('Github')}
+    >
       <FaGithub size={20} color='#eba417' />
       Sign In with Github
     </button>
